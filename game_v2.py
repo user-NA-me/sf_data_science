68edcd62334d102d27ad4e109c19cc7e3e60be0b
PurpleFlame <AleksandrSergeev75@outlook.com>
2023-01-26 20:29:44 +0500

    Add history of predicted numbers

--- game_v2.py
+++ game_v2.py
@@ -11,17 +11,22 @@ def random_predict(number:int=1) -> int:
     """
 
     count = 0
-
+    supposed_numbers_ls = []
     while True:
         count += 1
         predict_number = np.random.randint(1, 101) # assumed number
-        if number == predict_number:
+        if predict_number in [supposed_numbers_ls]:
+            continue
+        elif number == predict_number:
             break # exit from loop, if number were guessed
+        else:
+            supposed_numbers_ls.append(predict_number)
     return(count)
 
 print(f'Amount of tries: {random_predict()}')
 def score_game(random_predict) -> int:
-    """For how many attempts on average out of 1000 approaches our algorithm guesses
+    """For how many attempts on average out of 1000 approaches our algorithm 
+    guesses set number
 
     Args:
         random_predict ([type]): Guessing function
@@ -41,7 +46,7 @@ def score_game(random_predict) -> int:
 
     print(f'Your algorithm guesses the number on average for: {score} tries')
     return(score)
-
+    
 # RUN
 if __name__ == '__main__':
     score_game(random_predict)
